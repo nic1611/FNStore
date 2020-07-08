3e8cebdf371345c0d1425b0ee5b0ca5e9e907d78
Nicolas <nisoffi@gmail.com>
2020-07-07 21:03:45 -0300

    Estilo das views finalizado

---
+++ FN.Store.UI/Models/TipoDeProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Models
+{
+    public class TipoDeProduto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
